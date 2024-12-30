4d2a3d8ab7a45ea278db6baecb81f1224d7b690b
WTTien <70801065+WTTien@users.noreply.github.com>
2024-12-30 17:40:07 +0800

    Detect if minute changes and carry out corresponding operations

--- time.py
+++ time.py
@@ -1,10 +1,20 @@
 import smbus
 import time
-import argparse
+from datetime import datetime
 
 bus = smbus.SMBus(1)
-
 DEVICE_ADDRESS = 0x20
+
+step_sequence = [0b1001, 0b1000, 0b1100, 0b0100, 0b0110, 0b0010, 0b0011, 0b0001]
+
+
+def setup():	
+	global minuteHandStepped
+	minuteHandStepped = 0
+	
+	global hourHandStepped
+	hourHandStepped = 0
+
 
 def write_pcf8575(state1, state2):
 	try:
@@ -12,39 +22,21 @@ def write_pcf8575(state1, state2):
 	except OSError as e:
 		print(f"I2C Write Error: {e}")
 
-
-def setup():
+def spin_motor(minuteStep, hourStep):
 	global step_sequence
-	step_sequence = [0b1001, 0b1000, 0b1100, 0b0100, 0b0110, 0b0010, 0b0011, 0b0001]
-	
-	global moved_step
-	moved_step = 0
-	
-	global minute
-	minute = 0
-    
-def spinMotor():
-	global step_sequence
-	global moved_step
-	global minute
-	
-	moved_step_lag = moved_step < (4096/60)*minute
-	
-	if moved_step_lag:
-		count = 69
-	else:
-		count = 68	
 
 	step = 0
 	
-	for i in range(count):
+	for i in range(max(minuteStep, hourStep)):
 		state1 = 0b00000000
 		state2 = 0b00000000
 
-		state1 |= (step_sequence[step] << 4)
-		state2 |= (step_sequence[step])
+		if (i < minuteStep):
+			state1 |= (step_sequence[step] << 4)
 
-		
+		if (i < hourStep):
+			state2 |= (step_sequence[step])
+
 		state1 |= 0x05
 
 		write_pcf8575(state1, state2)
@@ -52,13 +44,64 @@ def spinMotor():
 		time.sleep(0.005)
 
 		step = (step + 1) % 8
-		
+
+
+def move_hands_to(minute, hour):
+	global minuteHandStepped
+	global hourHandStepped
+
+	# This is comparing the past minute
+	if minute == 0:
+		minuteAdjust = 59 # 60 - 1
+	else:
+		minuteAdjust = minute - 1
+	minuteHandLag = minuteHandStepped < ((4096/60) * minuteAdjust)
+
+	if minuteHandLag :
+		minuteOutput = 69
+	else:
+		minuteOutput = 68
+
+	# This is comparing the past minute
+	if (hour % 12) == 0 and minute == 0:
+		hourAdjust = 719 # 0 + 60*12 - 1
+	else:
+		hourAdjust = minute + (60*(hour % 12)) - 1
+	hourHandLag = hourHandStepped < ((4096/720) * hourAdjust)
+
+	if hourHandLag :
+		hourOutput = 6
+	else:
+		hourOutput = 5
+
+	spin_motor(minuteOutput, hourOutput)
+	write_pcf8575(0x05, 0x00)
+
+	minuteHandStepped = minuteHandStepped + minuteOutput
+	if minuteHandStepped == 4096:
+		minuteHandStepped = 0
 	
-	moved_step = moved_step + count
+	hourHandStepped = hourHandStepped + hourOutput
+	if hourHandStepped == 4096:
+		hourHandStepped = 0
+
+def check_time():
+
+	prev_minute = datetime.now().minute
+	
+	while True:
+		curr_minute = datetime.now().minute
+		curr_hour = datetime.now().hour
+		
+		if curr_minute != prev_minute:
+			move_hands_to(curr_minute, curr_hour)
+			prev_minute = curr_minute
+
+		time.sleep(1)
+
 
 
 if __name__ == "__main__":
-	
 	setup()
-	spinMotor(args.direction, args.state1, args.state2)
-	write_pcf8575(0x05, 0x00)
+	check_time()
+	
