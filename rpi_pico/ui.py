8858fb47790cd8f939be7368f7dfcefef30024b5
WTTien <wtiantien@hotmail.com>
2025-11-23 16:56:51 +0800

    Some styling change - button now becomes more informative and interactive. Minor style updates.

--- rpi_pico/ui.py
+++ rpi_pico/ui.py
@@ -1,6 +1,6 @@
 import sys
 import serial
-from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QHBoxLayout, QGridLayout, QVBoxLayout, QTextEdit
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout, QGridLayout, QVBoxLayout, QTextEdit, QSizePolicy
 from PyQt6.QtCore import QThread, pyqtSignal
 import time
 
@@ -67,34 +67,16 @@ class PicoWindow(QWidget):
         self.led_buttons = {} # Dictionary to store button references
 
         for led_id, row, col in led_buttons:
-            button = QPushButton(f"LED {led_id} - OFF")
+            button = QPushButton(f"LED {led_id}\nOFF")
+            button.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)  # Make button fill the grid cell
             button.clicked.connect(lambda checked, led_id=led_id: self.led_button_pressed(led_id))
             led_buttons_grid.addWidget(button, row, col)
             self.led_buttons[led_id] = button  # Store reference
-            
-        # self.led_1A_button = QPushButton("Switch LED 1A ON")
-        # self.led_1A_button.clicked.connect(self.led_1A_button_pressed)
-        # led_buttons_grid.addWidget(self.led_1A_button, 1, 1)
+        # for i in range(1, 4):  # Assuming 3 rows
+        #     led_buttons_grid.setRowStretch(i, 1)
+        # for j in range(1, 3):  # Assuming 2 columns
+        #     led_buttons_grid.setColumnStretch(j, 1)
 
-        # self.led_2A_button = QPushButton("Switch LED 2A ON")
-        # self.led_2A_button.clicked.connect(self.led_2A_button_pressed)
-        # led_buttons_grid.addWidget(self.led_2A_button, 1, 2)
-
-        # self.led_1B_button = QPushButton("Switch LED 1B ON")
-        # self.led_1B_button.clicked.connect(self.led_1B_button_pressed)
-        # led_buttons_grid.addWidget(self.led_1B_button, 2, 1)
-
-        # self.led_2B_button = QPushButton("Switch LED 2B ON")
-        # self.led_2B_button.clicked.connect(self.led_2B_button_pressed)
-        # led_buttons_grid.addWidget(self.led_2B_button, 2, 2)
-
-        # self.led_1C_button = QPushButton("Switch LED 1C ON")
-        # self.led_1C_button.clicked.connect(self.led_1C_button_pressed)
-        # led_buttons_grid.addWidget(self.led_1C_button, 3, 1)
-        
-        # self.led_2C_button = QPushButton("Switch LED 2C ON")
-        # self.led_2C_button.clicked.connect(self.led_2C_button_pressed)
-        # led_buttons_grid.addWidget(self.led_2C_button, 3, 2)
 
         controls_layout.addWidget(led_controls_layout)
         controls_widget.setLayout(controls_layout)
@@ -109,79 +91,46 @@ class PicoWindow(QWidget):
 
     def led_button_pressed(self, led_id):
         try:
-            # Send a test command
-            self.append_output(f">> Switching LED {led_id}")
-            self.serial_thread.ser.write(f"[LED] {led_id} : ON\n".encode())
-            self.serial_thread.ser.flush()
+            button = self.led_buttons.get(led_id)
+            if not button:
+                print(f"No button found for LED {led_id}")
+                self.append_output(f"Error: No button found for LED {led_id}")
+                return
+            
+            # User pressed - LED now becomes OFF
+            if "ON" in button.text():
+                try:
+                    self.serial_thread.ser.write(f"[LED] {led_id} : OFF\n".encode())
+                    self.serial_thread.ser.flush()
+                    self.append_output(f">>> Switching LED {led_id} OFF")
+                except Exception as e:
+                    self.append_output(f"Error communicating with Pico: {e}")
+                    return
+                button.setStyleSheet("") # Now LED OFF so background back to default
+                button.setText(f"LED {led_id}\nOFF") # User can switch it ON next
+
+            # User pressed - LED now becomes ON
+            elif "OFF" in button.text():
+                try:
+                    self.serial_thread.ser.write(f"[LED] {led_id} : ON\n".encode())
+                    self.serial_thread.ser.flush()
+                    self.append_output(f">>> Switching LED {led_id} ON")
+                except Exception as e:
+                    self.append_output(f"Error communicating with Pico: {e}")
+                    return
+                button.setStyleSheet("background-color: green;") # Now LED ON so background green
+                button.setText(f"LED {led_id}\nON") # User can switch it OFF next
+
         except Exception as e:
-            self.append_output(f"Error sending to Pico: {e}")
-
-    # def led_1A_button_pressed(self):
-    #     try:
-    #         # Send a test command
-    #         self.append_output(">> Switching LED 1A")
-    #         self.serial_thread.ser.write(b"[LED] 1A : ON\n")
-    #         self.serial_thread.ser.flush()
-    #     except Exception as e:
-    #         self.append_output(f"Error sending to Pico: {e}")
-
-    # def led_1B_button_pressed(self):
-    #     try:
-    #         # Send a test command
-    #         self.append_output(">> Switching LED 1B")
-    #         self.serial_thread.ser.write(b"[LED] 1B : ON\n")
-    #         self.serial_thread.ser.flush()
-    #     except Exception as e:
-    #         self.append_output(f"Error sending to Pico: {e}")
-
-    # def led_2A_button_pressed(self):
-    #     try:
-    #         # Send a test command
-    #         self.append_output(">> Switching LED 2A")
-    #         self.serial_thread.ser.write(b"[LED] 2A : ON\n")
-    #         self.serial_thread.ser.flush()
-    #     except Exception as e:
-    #         self.append_output(f"Error sending to Pico: {e}")
-
-    # def led_2B_button_pressed(self):
-    #     try:
-    #         # Send a test command
-    #         self.append_output(">> Switching LED 2B")
-    #         self.serial_thread.ser.write(b"[LED] 2B : ON\n")
-    #         self.serial_thread.ser.flush()
-    #     except Exception as e:
-    #         self.append_output(f"Error sending to Pico: {e}")
-
-    # def led_1C_button_pressed(self):
-    #     try:
-    #         # Send a test command
-    #         self.append_output(">> Switching LED 1C")
-    #         self.serial_thread.ser.write(b"[LED] 1C : ON\n")
-    #         self.serial_thread.ser.flush()
-    #     except Exception as e:
-    #         self.append_output(f"Error sending to Pico: {e}")
-
-    # def led_2C_button_pressed(self):
-    #     try:
-    #         # Send a test command
-    #         self.append_output(">> Switching LED 2C")
-    #         self.serial_thread.ser.write(b"[LED] 2C : ON\n")
-    #         self.serial_thread.ser.flush()
-    #     except Exception as e:
-    #         self.append_output(f"Error sending to Pico: {e}")
-
-    # def send_to_pico(self):
-    #     try:
-    #         # Send a test command
-    #         self.serial_thread.ser.write(b"PING\n")
-    #         self.serial_thread.ser.flush()
-    #         self.append_output(">> Sent: PING")
-    #     except Exception as e:
-    #         self.append_output(f"Error sending to Pico: {e}")
+            self.append_output(f"Error: {e}")
 
     # Callback to update output box
     def append_output(self, text):
-        self.output_box.append(text)
+        if text.startswith(">>>"):
+            user_input = f'<span style="color: orange;">{text}</span>'
+            self.output_box.append(user_input)
+        else:
+            self.output_box.append(text)
 
     def closeEvent(self, event):
         self.serial_thread.stop()
